4dd25d0d2d4e3b36906624a689bb520dce13dc99
Aaryan <aaryany.2505@gmail.com>
2024-03-12 12:15:51 +0530

    Maid Loader

---
+++ script.js
@@ -0,0 +1,50 @@
+function Counter(){
+    var timer = document.querySelector("#part1 h5");
+    var count = 0
+    
+    setInterval(function(){
+        if(count<100){
+            
+            timer.innerHTML = count++
+        }
+        else{
+            timer.innerHTML = count
+        }
+    },33)
+}
+
+
+var tl =gsap.timeline()
+tl.from(".line h1",{
+    y :150,
+duration:0.5,
+
+opacity:0,
+stagger:0.3
+})
+tl.from("#part1",{
+    opacity:0,
+   OnStart:Counter()
+    
+})
+tl.to(".line h2",{
+    animationName :"anime",
+    opacity:0
+}
+)
+tl.to("#loader", {
+    opacity: 0,
+    duration: 0.1,
+    delay: 3,
+  });
+
+tl.from("#page1",{
+  delay:0.2,
+    y:1200,
+    opacity:1,
+    ease:Power4
+})
+tl.to("#loader",{
+    display:"none"
+})
+
