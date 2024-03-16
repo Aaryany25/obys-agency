346062bf6c21a543ebf4da111cda871d41d29ca3
Aaryan <aaryany.2505@gmail.com>
2024-03-17 00:52:37 +0530

    Created Page1

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-function Counter(){
+function LoadingAnimation() {
+  function Counter() {
     var timer = document.querySelector("#part1 h5");
-    var count = 0
-    
-    setInterval(function(){
-        if(count<100){
-            
-            timer.innerHTML = count++
-        }
-        else{
-            timer.innerHTML = count
-        }
-    },33)
-}
+    var count = 0;
 
+    setInterval(function () {
+      if (count < 100) {
+        timer.innerHTML = count++;
+      } else {
+        timer.innerHTML = count;
+      }
+    }, 33);
+  }
 
-var tl =gsap.timeline()
-tl.from(".line h1",{
-    y :150,
-duration:0.5,
+  var tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 150,
+    duration: 0.5,
 
-opacity:0,
-stagger:0.3
-})
-tl.from("#part1",{
-    opacity:0,
-   OnStart:Counter()
-    
-})
-tl.to(".line h2",{
-    animationName :"anime",
-    opacity:0
-}
-)
-tl.to("#loader", {
+    opacity: 0,
+    stagger: 0.3,
+  });
+  tl.from("#part1", {
+    opacity: 0,
+    OnStart: Counter(),
+  });
+  tl.to(".line h2", {
+    animationName: "anime",
+    opacity: 0,
+  });
+  tl.to("#loader", {
     opacity: 0,
     duration: 0.1,
     delay: 3,
   });
 
-tl.from("#page1",{
-  delay:0.2,
-    y:1200,
-    opacity:1,
-    ease:Power4
-})
-tl.to("#loader",{
-    display:"none"
-})
-
+  tl.from("#page1", {
+    delay: 0.2,
+    y: 1200,
+    opacity: 1,
+    ease: Power4,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+function CursurAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+  Shery.makeMagnet("#navPart2 h3", {});
+}
+LoadingAnimation();
+CursurAnimation();
